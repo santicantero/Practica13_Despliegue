021926cdd1a89a85ecc0f0bef9f3c5f37b3e4e2b
santicantero <pepesperez4@gmail.com>
2026-02-18 11:56:42 +0100

    primer commit santiago cantero torrents

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,24 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Calculadora",
+            "name": "Calculadora",
+            "summary": "Clase\u0020Calculadora",
+            "url": "classes/Calculadora.html"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Asumar\u0028\u0029",
+            "name": "sumar",
+            "summary": "Suma\u0020dos\u0020n\u00FAmeros.",
+            "url": "classes/Calculadora.html#method_sumar"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Arestar\u0028\u0029",
+            "name": "restar",
+            "summary": "Resta\u0020dos\u0020n\u00FAmeros.",
+            "url": "classes/Calculadora.html#method_restar"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
---
+++ ejemplo.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Clase Calculadora
+ *
+ * Clase simple para realizar operaciones matemáticas básicas.
+ */
+class Calculadora
+
+//Comentario para subir proyecto al git
+
+{
+    /**
+     * Suma dos números.
+     *
+     * @param int $a Primer número
+     * @param int $b Segundo número
+     * @return int Resultado de la suma
+     */
+    public function sumar(int $a, int $b): int
+    {
+        return $a + $b;
+    }
+
+    /**
+     * Resta dos números.
+     *
+     * @param int $a Primer número
+     * @param int $b Segundo número
+     * @return int Resultado de la resta
+     */
+    public function restar(int $a, int $b): int
+    {
+        return $a - $b;
+    }
+}
